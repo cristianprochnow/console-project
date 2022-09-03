2e159d6d14940e6a5bfb734af680a7b37381cb3e
Cristian Prochnow <cristianprochnow@gmail.com>
2022-09-02 22:20:53 -0300

    feat: update Clients

--- src/class/Clients.cs
+++ src/class/Clients.cs
@@ -1,15 +1,48 @@
 class Clients
 {
   private Screen screen;
+  private List<ClientsData> data = new List<ClientsData>();
 
   public Clients(Screen screen)
   {
     this.screen = screen;
+
+    this.data.Add(new ClientsData("1", "Xunda", "x@b.com", "123"));
+    this.data.Add(new ClientsData("2", "Babalu", "b@b.com", "456"));
   }
 
   public void executeCRUD()
   {
-    this.buildScreen(10, 8);
+    string id, name, email, phone;
+    int initialColumn, initialRow;
+
+    initialColumn = 10;
+    initialRow = 8;
+
+    this.buildScreen(initialColumn, initialRow);
+
+    initialColumn += 11;
+
+    Console.SetCursorPosition(initialColumn, initialRow + 2);
+    id = Console.ReadLine();
+
+    bool hasFound = false;
+    for (int counter = 0; counter < this.data.Count; counter++)
+    {
+      if (this.data[counter].id == id) {
+        hasFound = true;
+        break;
+      }
+    }
+
+    Console.SetCursorPosition(initialColumn, initialRow + 3);
+    if (hasFound) {
+      Console.Write("Código já registrado!");
+    }
+    else {
+      Console.Write("Código ainda não usado!");
+    }
+    Console.ReadKey();
   }
 
   public void buildScreen(int initialColumn, int initialRow)
@@ -21,6 +54,11 @@ class Clients
 
     this.screen.buildFrame(initialColumn, initialRow, finalColumn, finalRow);
     this.screen.insert(initialColumn + 1, initialRow + 1, "Cadastro de Clientes");
+    this.screen.insert(initialColumn + 1, initialRow + 2, "Código:        ");
+    this.screen.insert(initialColumn + 1, initialRow + 3, "Nome:          ");
+    this.screen.insert(initialColumn + 1, initialRow + 4, "Email:         ");
+    this.screen.insert(initialColumn + 1, initialRow + 5, "Telefone:      ");
+    this.screen.insert(initialColumn + 1, initialRow + 6, "Confirma (S/N):");
     Console.ReadKey();
   }
 }
---
+++ src/db/ClientsData.cs
@@ -0,0 +1,12 @@
+class ClientsData
+{
+  public string id, name, email, phone;
+
+  public ClientsData(string id, string name, string email, string phone)
+  {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+  }
+}
